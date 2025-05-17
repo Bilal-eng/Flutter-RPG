a0883dba9ce3ceb7f013d97e7a3d3b27aa3b821a
Bilal-eng <bilal.haliloglu90@gmail.com>
2025-05-17 18:14:18 +0300

    Making a Skill Class

--- lib/models/vocation.dart
+++ lib/models/vocation.dart
@@ -1,31 +1,31 @@
 enum Vocation {
   raider(
-    title: "Terminal Raider", 
+    title: "Terminal Raider",
     description: "Adept in terminal commands to trigger traps.",
     weapon: "Terminal",
     ability: "Shellshock",
-    image: "terminal_raider.jpg"
+    image: "terminal_raider.jpg",
   ),
   junkie(
-    title: "Code Junkie", 
+    title: "Code Junkie",
     description: "Uses code to infiltrate enemy defenses.",
     weapon: "React 99",
     ability: "Higher Order Overdrive",
-    image: "code_junkie.jpg"
+    image: "code_junkie.jpg",
   ),
   ninja(
-    title: "UX Ninja", 
+    title: "UX Ninja",
     description: "Uses quick & stealthy visual attacks.",
     weapon: "Infused Stylus",
     ability: "Triple Swipe",
-    image: "ux_ninja.jpg"
+    image: "ux_ninja.jpg",
   ),
   wizard(
-    title: "Algo Wizard", 
+    title: "Algo Wizard",
     description: "Carries a staff to unleash algorithm magic.",
     weapon: "Crystal Staff",
     ability: "Algorythmic Daze",
-    image: "algo_wizard.jpg"
+    image: "algo_wizard.jpg",
   );
 
   const Vocation({
@@ -33,12 +33,12 @@ enum Vocation {
     required this.description,
     required this.image,
     required this.weapon,
-    required this.ability 
+    required this.ability,
   });
 
   final String title;
   final String description;
   final String image;
   final String weapon;
-  final String ability
+  final String ability;
 }
