36a05375467978daecd3a8bdc4b6425de037e62d
Bilal-eng <bilal.haliloglu90@gmail.com>
2025-05-20 21:56:38 +0300

    Using Tables

--- lib/screens/profile/stats_table.dart
+++ lib/screens/profile/stats_table.dart
@@ -36,6 +36,69 @@ class _StatsTableState extends State<StatsTable> {
               ],
             ),
           ),
+
+          // stats table
+          Table(
+            children:
+                widget.character.statsAsFormattedList.map((stat) {
+                  return TableRow(
+                    decoration: BoxDecoration(
+                      color: AppColors.secondaryColor.withValues(alpha: 0.5),
+                    ),
+                    children: [
+                      // stat title
+                      TableCell(
+                        verticalAlignment: TableCellVerticalAlignment.middle,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: StyledHeading(stat['title']!),
+                        ),
+                      ),
+
+                      // stat value
+                      TableCell(
+                        verticalAlignment: TableCellVerticalAlignment.middle,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: StyledHeading(stat['value']!),
+                        ),
+                      ),
+
+                      // icon to increase stat
+                      TableCell(
+                        verticalAlignment: TableCellVerticalAlignment.middle,
+                        child: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              widget.character.increaseStat(stat['title']!);
+                            });
+                          },
+                          icon: Icon(
+                            Icons.arrow_upward,
+                            color: AppColors.textColor,
+                          ),
+                        ),
+                      ),
+
+                      // icon to decrease stat
+                      TableCell(
+                        verticalAlignment: TableCellVerticalAlignment.middle,
+                        child: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              widget.character.decreaseStat(stat['title']!);
+                            });
+                          },
+                          icon: Icon(
+                            Icons.arrow_downward,
+                            color: AppColors.textColor,
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                }).toList(),
+          ),
         ],
       ),
     );
