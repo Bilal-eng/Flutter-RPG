3ead76b7861da600975f8f8b7747ec4de036ec0a
Bilal-eng <bilal.haliloglu90@gmail.com>
2025-05-20 11:54:05 +0300

    Passing Data into Routes

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -1,18 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rpg/models/character.dart';
 import 'package:flutter_rpg/shared/styled_text.dart';
+import 'package:flutter_rpg/theme.dart';
 
 class Profile extends StatelessWidget {
-  const Profile({super.key});
+  const Profile({super.key, required this.character});
+
+  final Character character;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const StyledTitle('character name')),
-      body: const SingleChildScrollView(
+      appBar: AppBar(title: StyledTitle(character.name)),
+      body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             // basic info - image, vocation, description
+            Container(
+              padding: const EdgeInsets.all(16),
+              color: AppColors.secondaryColor.withValues(alpha: 0.3),
+              child: Row(
+                children: [
+                  Image.asset(
+                    'assets/img/vocations/${character.vocation.image}',
+                    width: 140,
+                    height: 140,
+                  ),
+                  const SizedBox(width: 20),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        StyledHeading(character.vocation.title),
+                        StyledText(character.vocation.description),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
 
             // weapon and ability
 
