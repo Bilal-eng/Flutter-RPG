c61333a2f6c16d349197e1599bcb4623a2c5ac4a
Bilal-eng <bilal.haliloglu90@gmail.com>
2025-05-23 12:41:28 +0300

    Animated Rotation

--- lib/screens/profile/stats_table.dart
+++ lib/screens/profile/stats_table.dart
@@ -12,6 +12,8 @@ class StatsTable extends StatefulWidget {
 }
 
 class _StatsTableState extends State<StatsTable> {
+  double turns = 0.0;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -24,10 +26,16 @@ class _StatsTableState extends State<StatsTable> {
             padding: const EdgeInsets.all(8),
             child: Row(
               children: [
-                Icon(
-                  Icons.star,
-                  color:
-                      widget.character.points > 0 ? Colors.yellow : Colors.grey,
+                AnimatedRotation(
+                  turns: turns,
+                  duration: const Duration(milliseconds: 200),
+                  child: Icon(
+                    Icons.star,
+                    color:
+                        widget.character.points > 0
+                            ? Colors.yellow
+                            : Colors.grey,
+                  ),
                 ),
                 const SizedBox(width: 20),
                 const StyledText('Stats points available: '),
@@ -71,6 +79,7 @@ class _StatsTableState extends State<StatsTable> {
                           onPressed: () {
                             setState(() {
                               widget.character.increaseStat(stat['title']!);
+                              turns += 0.5;
                             });
                           },
                           icon: Icon(
@@ -87,6 +96,7 @@ class _StatsTableState extends State<StatsTable> {
                           onPressed: () {
                             setState(() {
                               widget.character.decreaseStat(stat['title']!);
+                              turns -= 0.5;
                             });
                           },
                           icon: Icon(
