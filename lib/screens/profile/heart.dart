7fc28aa909b2732b58747fa61bee47feb35fe8cd
Bilal-eng <bilal.haliloglu90@gmail.com>
2025-05-23 15:20:44 +0300

    Animated Builder Widget

--- lib/screens/profile/heart.dart
+++ lib/screens/profile/heart.dart
@@ -31,11 +31,22 @@ class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      onPressed: () {
-        widget.character.toggleIsFav();
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return IconButton(
+          onPressed: () {
+            _controller.reset();
+            _controller.forward();
+            widget.character.toggleIsFav();
+          },
+          icon: Icon(
+            Icons.favorite,
+            color: Colors.grey[800],
+            size: _sizeAnimation.value,
+          ),
+        );
       },
-      icon: Icon(Icons.favorite, color: Colors.grey[800]),
     );
   }
 }
