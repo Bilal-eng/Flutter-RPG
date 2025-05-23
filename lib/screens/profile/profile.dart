768f95c49d2d088c916617c9d11b4e9069af5371
Bilal-eng <bilal.haliloglu90@gmail.com>
2025-05-23 12:56:30 +0300

    Animation Controllers

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rpg/models/character.dart';
+import 'package:flutter_rpg/screens/profile/heart.dart';
 import 'package:flutter_rpg/screens/profile/skill_list.dart';
 import 'package:flutter_rpg/screens/profile/stats_table.dart';
 import 'package:flutter_rpg/services/character_store.dart';
@@ -48,6 +49,8 @@ class Profile extends StatelessWidget {
                 ],
               ),
             ),
+
+            Heart(character: character),
 
             // weapon and ability and slogan
             const SizedBox(height: 20),
